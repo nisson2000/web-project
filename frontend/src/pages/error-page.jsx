e8309d9a35f8a7ed2cb4667ee0122cbfb3233786
Nisson <nisson2000@gmail.com>
2024-04-12 15:08:55 +0800

    0412

--- frontend/src/pages/error-page.jsx
+++ frontend/src/pages/error-page.jsx
@@ -7,7 +7,7 @@ export default function ErrorPage() {
   return (
     <div id="error-page">
       <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
+      <p>Sorry, an unexpected error has occurred</p>
       <p>
         <i>
           {error.status} {error.data || error.statusText || error.message}
