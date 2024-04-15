ecbbef4b16b01c98c577ad5110255ab22f109e24
Nisson <nisson2000@gmail.com>
2024-04-15 16:45:28 +0800

    0415

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import RootLayout, { RootIndex } from "./pages";
 import Index from "./pages/index";
 import About from "./pages/about";
@@ -9,7 +9,7 @@ import UserPage from "./pages/users";
 import CreateUserPage from "./pages/create-user";
 import ErrorPage from "./pages/error-page";
 
-const router = createBrowserRouter([
+/*const router = createBrowserRouter([
   {
     path: "",
     element: <RootLayout />,
@@ -32,10 +32,19 @@ const router = createBrowserRouter([
   },
 ], {
   basename: '/web_project/',  // Set the base path for all routes
-});
+});*/
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<RootLayout />} errorElement={<ErrorPage />}>
+          <Route index element={<RootIndex />} />
+          <Route path="about" element={<About />} />
+          <Route path="users" element={<UserPage />} />
+          <Route path="create-user" element={<CreateUserPage />} />
+        </Route>
+      </Routes>
+    </HashRouter>
   </React.StrictMode>
 );
