bd98bcd105f7e2bb19de388c6615f06b9e173fba
Nisson <nisson2000@gmail.com>
2024-04-15 18:39:26 +0800

    0415

--- frontend/src/pages/create-user.jsx
+++ frontend/src/pages/create-user.jsx
@@ -76,21 +76,45 @@ function CreateUserPage() {
               Create an account
             </h2>
           </div>
-          <form onSubmit={handleFormSubmit}>
-            <label>
-              Username:
-              <input type="text" name="username" value={formData.username} onChange={handleTextInputChange} />
-            </label>
-            <label>
-              Password:
-              <input type="password" name="password" value={formData.password} onChange={handleTextInputChange} />
-            </label>
-            <label>
-              Upload Avatar (JPG, PNG):
-              <input type="file" accept=".jpg, .jpeg, .png" onChange={handleFileChange} />
-            </label>
-            <button type="submit">Register</button>
-            <pre>{message}</pre>
+          <form className="mt-8 space-y-6" onSubmit={handleFormSubmit}>
+            <div className="-space-y-px rounded-md shadow-sm">
+              <div>
+                <label>
+                  Username:
+                  <input type="text" name="username" value={formData.username} onChange={handleTextInputChange} />
+                </label>
+              </div>
+            </div>
+            <div className="-space-y-px rounded-md shadow-sm">
+              <div>
+                <label>
+                  Password:
+                  <input type="password" name="password" value={formData.password} onChange={handleTextInputChange} />
+                </label>            
+              </div>
+            </div>
+            <div className="-space-y-px rounded-md shadow-sm">
+              <div>
+                <label>
+                  Upload Avatar (JPG, PNG):
+                  <input type="file" accept=".jpg, .jpeg, .png" onChange={handleFileChange} />
+                </label>
+              </div>
+            </div>
+            <div>
+              <button
+                type="submit"
+                className="group relative flex w-full justify-center rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                  <LockClosedIcon
+                    className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+                    aria-hidden="true"
+                  />
+                </span>
+                Create
+              </button>
+            </div>
           </form>
         </div>
       </div>
