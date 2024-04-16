3553fb59fc404437262e755e88d0f5dced42af06
Nisson <nisson2000@gmail.com>
2024-04-16 10:33:16 +0800

    0416

--- frontend/src/pages/create-user.jsx
+++ frontend/src/pages/create-user.jsx
@@ -73,7 +73,7 @@ function CreateUserPage() {
         <div className="w-full max-w-md space-y-8">
           <div>
             <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
-              Create an Account
+              Create an account
             </h2>
           </div>
           <form className="mt-8 space-y-6" onSubmit={handleFormSubmit}>
